6ce229a823d864824ae7ce79d26999c989b0070e
RachaelDugan <118020235+RachaelDugan@users.noreply.github.com>
2023-01-03 13:26:30 -0700

    hope I set the appsetting.json up right

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using BestBuyBestPractices;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+string connString = config.GetConnectionString("DefaultConnection");
+
+IDbConnection conn = new MySqlConnection(connString);
+
+var departmentRepo = new DapperDepartmentRepository(conn);
+
+departmentRepo.InsertDepartment("Car Audio");
+
+var departments = departmentRepo.GetAllDepartments();
+
+foreach (var department in departments)
+{
+    Console.WriteLine(department.DepartmentID);
+    Console.WriteLine(department.Name);
+    Console.WriteLine();
+    Console.WriteLine();
+}
